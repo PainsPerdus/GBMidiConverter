b6518f4c61c5b36dec4e50c9ab1f305f5f421e48
DorianXGH <dorian.bourgeoisat@icloud.com>
2020-06-25 17:26:16 +0200

    added event and delay decoding

--- include/main.h
+++ include/main.h
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 struct MThd {
     uint32_t magic;
@@ -9,10 +10,10 @@ struct MThd {
     uint16_t SMF;
     uint16_t tracks;
     uint16_t bpm;
-} __attribute__((scalar_storage_order ("big-endian")));
+} __attribute__((packed, scalar_storage_order ("big-endian")));
 struct MTrk {
     uint32_t magic;
     uint32_t size;
-} __attribute__((scalar_storage_order ("big-endian")));
+} __attribute__((packed, scalar_storage_order ("big-endian")));
 
 void convert(FILE* infile, FILE* outfile);
--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@ void convert(FILE* infile, FILE* outfile)
     uint8_t notes[4096];
     uint8_t head;
     struct MThd hd;
+    bool delayMode = true;
     fread(&hd,sizeof(hd),1,infile);
     printf("//---  Header  ---\\\\\n");
     printf("  Magic : %X\n",hd.magic);
@@ -26,10 +27,60 @@ void convert(FILE* infile, FILE* outfile)
         struct MTrk trk;
         fread(&trk,sizeof(trk),1,infile);
         printf("//--  Track %i  --\\\\\n", i);
+        printf("  Magic : %X\n",trk.magic);
         printf("  Size : %i\n",trk.size);
-        for(int j = 0; j < trk.size-3; j++)
+        uint32_t delay = 0;
+        uint8_t status = 0;
+        uint32_t ignore = 0;
+        for(int j = 0; j < trk.size-ignore-3; j++)
         { 
             head = getc(infile);
+            if(delayMode) 
+            {
+                if(head > 127)
+                {
+                    delay = delay * 128 + (head-128);
+                }
+                else
+                {
+                    delay = delay * 128 + head;
+                    delayMode = false;
+                }
+            }
+            else
+            {
+                if(head > 127)
+                {
+                    status = head;
+                }
+                
+                uint8_t function = status & 0xF0;
+                uint8_t channel = status & 0x0F;
+                uint8_t note = 0;
+                uint8_t velo = 0;
+                switch(function)
+                {
+                    case 0x90 : 
+                        note = getc(infile);
+                            velo = getc(infile);
+                            printf("Note %hhX ON, delay : %i, channel : %hhi \n",note, delay, channel);
+                            ignore += 2;
+                            break;
+                        case 0x80 : 
+                            note = getc(infile);
+                            velo = getc(infile);
+                            printf("Note %hhX OFF, delay : %i, channel : %hhi \n",note, delay, channel);
+                            ignore += 2;
+                            break;
+                        default:
+                            printf("Event not known, delay : %i \n",delay);
+                            getc(infile);
+                            getc(infile);
+                            ignore += 2;
+                            break;
+                    } 
+                
+            }
         }
         printf("%hhX %hhX %hhX \n",getc(infile),getc(infile),getc(infile));
         printf("\\\\--  Track %i  --//\n", i);
